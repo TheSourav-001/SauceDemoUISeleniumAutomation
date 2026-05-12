bfaaa71c408110e8aa3f57e24b8ec00a6f642273
TheSourav-001 <shouravdiptoshillapu@gmail.com>
2026-05-13 04:17:13 +0600

    Enhance test framework with page object model and reporting features

--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -8,13 +8,11 @@ import java.time.Duration;
 
 public class WaitUtils {
 
-    // এই মেথডটা একটা ইলিমেন্ট স্ক্রিনে দৃশ্যমান হওয়া পর্যন্ত ওয়েট করবে
     public static void waitForVisibility(WebDriver driver, WebElement element, int timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    // এই মেথডটা একটা বাটনে ক্লিক করার উপযোগী হওয়া পর্যন্ত ওয়েট করবে
     public static void waitForClickable(WebDriver driver, WebElement element, int timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         wait.until(ExpectedConditions.elementToBeClickable(element));
--- src/test/java/tests/StandardUserJourneyTest.java
+++ src/test/java/tests/StandardUserJourneyTest.java
@@ -1,4 +1,69 @@
 package tests;
 
-public class StandardUserJourneyTest {
+import base.BaseTest;
+import com.aventstack.extentreports.Status;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.CheckoutPage;
+import pages.InventoryPage;
+import pages.LoginPage;
+
+public class StandardUserJourneyTest extends BaseTest {
+
+    @Test
+    public void standardUserJourney() throws InterruptedException {
+
+        test = extent.createTest("Q2: Standard User Journey", "Testing the standard user journey from login to checkout");
+
+        LoginPage loginPage = new LoginPage(driver);
+        InventoryPage inventory = new InventoryPage(driver);
+        CartPage cart = new CartPage(driver);
+        CheckoutPage checkout = new CheckoutPage(driver);
+
+        test.log(Status.INFO, "Logging in with standard_user credentials");
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("secret_sauce");
+        loginPage.clickLoginButton();
+
+        test.log(Status.INFO, " Open hamburger menu and reset the app state ");
+        inventory.clickHamburger();
+        inventory.clickResetAppState();
+        inventory.clickCrossIcon();
+
+        test.log(Status.INFO, "Adding total 3 products to the cart");
+        inventory.addProduct01ToCart();
+        inventory.addProduct02ToCart();
+        inventory.addProduct03ToCart();
+
+        test.log(Status.INFO, "Navigating to the cart");
+        inventory.clickToCart();
+
+        test.log(Status.INFO, "Proceeding to checkout");
+        cart.ClickCheckout();
+
+        test.log(Status.INFO, "Entering checkout details");
+        checkout.enterFirstName("Sourav Dipto");
+        checkout.enterLastName("Apu");
+        checkout.enterPostalCode("12345");
+        checkout.clickContinueButton();
+
+        test.log(Status.INFO, "Verifying Product Name and Total Price");
+        Assert.assertEquals(checkout.getFirstProductName(), "Sauce Labs Bike Light", "Product name doesn't match!");
+        Assert.assertEquals(checkout.getTotalPrice(), "Total: $82.05", "Price doesn't match!");
+
+        test.log(Status.INFO, "Finishing the checkout process");
+        checkout.clickFinishButton();
+
+        test.log(Status.INFO, "Verifying successful order message");
+        Assert.assertEquals(checkout.getSuccessMessage(), "Thank you for your order!", "Success message not found!");
+
+        test.log(Status.INFO, "Re open the hamburger menu for reset the app state and logout");
+        inventory.clickHamburger();
+        inventory.clickResetAppState();
+        inventory.clickCrossIcon();
+        inventory.logout();
+
+    }
+
 }
